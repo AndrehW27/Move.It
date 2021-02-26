29928796f0b6cbb02dd759aee965e50da3c624bf
Andre Willian <andrehwillian@hotmail.com>
2021-02-26 19:00:40 -0300

    Dia 5

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -6,34 +6,58 @@ import { ChallengeBox } from "../components/ChallengeBox";
 
 
 import Head from 'next/head';
+import { GetServerSideProps } from 'next';
 
 import styles from '../styles/pages/Home.module.css';
 import { CountdownProvider } from "../contexts/CountdownContext";
+import { ChallengesProvider } from "../contexts/ChallengesContext";
 
+interface HomeProps{
+  level: number,
+  currentExperience: number,
+  challengesCompleted: number
+}
 
+export default function Home(props: HomeProps) {
+  console.log(props);
 
-
-export default function Home() {
   return (
+    <ChallengesProvider
+    level={props.level}
+    currentExperience={props.currentExperience} 
+    challengesCompleted={props.challengesCompleted}
+    >
+      <div className={styles.container}>
+        <Head>
+          <title>Início | move.it</title>
+        </Head>
+        <ExperienceBar />
 
-    <div className={styles.container}>
-      <Head>
-        <title>Início | move.it</title>
-      </Head>
-      <ExperienceBar />
-
-      <CountdownProvider>
-        <section >
-          <div>
-            <Profile />
-            <CompletedChallenges />
-            <Countdown />
-          </div>
-          <div>
-            <ChallengeBox />
-          </div>
-        </section>
-      </CountdownProvider>
-    </div>
+        <CountdownProvider>
+          <section >
+            <div>
+              <Profile />
+              <CompletedChallenges />
+              <Countdown />
+            </div>
+            <div>
+              <ChallengeBox />
+            </div>
+          </section>
+        </CountdownProvider>
+      </div>
+    </ChallengesProvider>
   )
+}
+
+export const getServerSideProps: GetServerSideProps = async (ctx) => {
+  const { level, currentExperience, challengesCompleted } = ctx.req.cookies;
+
+  return {
+    props: {
+      level: Number(level),
+      currentExperience: Number(currentExperience),
+      challengesCompleted: Number(challengesCompleted)
+    }
+  }
 }
